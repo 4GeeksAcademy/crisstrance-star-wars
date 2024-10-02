badec9b92418157b6d7e83fab5887c37290b3f7d
Cristian Aravena <50273105+crisstrance@users.noreply.github.com>
2024-10-02 20:03:18 +0000

    [EDIT] adding end points

--- src/api/models.py
+++ src/api/models.py
@@ -24,7 +24,7 @@ class Users(db.Model):
                 'email': self.email,
                 'is_active': self.is_active,
                 'is_admin': self.is_admin,
-                'first_name': self.firs_name,
+                'first_name': self.first_name,
                 'last_name': self.last_name,
                 'posts': [row.serialize() for row in self.posts_to]}
     
@@ -37,7 +37,7 @@ class Followers(db.Model):
     follower_to = db.relationship('Users', foreign_keys=[follower_id], backref=db.backref('follower_to', lazy='select'))
 
     def __repr__(self):
-        return f'Post: {self.id} - {self.title}'
+        return f'following: {self.following_id} - follower: {self.follower_id}'
                 
 
 class Posts(db.Model):
@@ -47,8 +47,8 @@ class Posts(db.Model):
     body = db.Column(db.String, unique=False, nullable=False)
     date = db.Column(db.DateTime, nullable=False) #Tomar valor por defecto
     image_url = db.Column(db.String) # url de Cloudinary
-    users_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    users_to = db.relationship('Users', foreign_keys=[users_id], backref=db.backref('posts_to', lazy='select'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('posts_to', lazy='select'))
 
     def __repr__(self):
         return f'Post: {self.id} - {self.title}'
@@ -60,7 +60,7 @@ class Posts(db.Model):
                 'body': self.body,
                 'date': self.date,
                 'image_url': self.image_url,
-                'users_id': self.users_id}
+                'user_id': self.user_id,}
     
 #     {
 #     "title": "Column",
@@ -139,6 +139,18 @@ class Planets(db.Model):
     climate = db.Column(db.String, unique=False, nullable=False)
     terrain = db.Column(db.String, unique=False, nullable=False)
 
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'name': self.name,
+                'diameter': self.diameter,
+                'rotation_period': self.rotation_period,
+                'orbital_period': self.orbital_period,
+                'gravity': self.gravity,
+                'population': self.population,
+                'climate': self.climate,
+                'terrain': self.terrain,}
+
 
 class PlanetFavorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -146,4 +158,31 @@ class PlanetFavorites(db.Model):
     users_to = db.relationship('Users', foreign_keys=[users_id], backref=db.backref('planetFavorites_to', lazy='select'))
     planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
     planet_to = db.relationship('Planets', foreign_keys=[planet_id], backref=db.backref('planetFavorites_to', lazy='select'))
-    
+
+
+class Starships(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=False, nullable=False)
+    passengers = db.Column(db.String, unique=False, nullable=False)
+    crew = db.Column(db.String, unique=False, nullable=False)
+    length = db.Column(db.String, unique=False, nullable=False)
+    consumables = db.Column(db.String, unique=False, nullable=False)
+    cargo_capacity = db.Column(db.String, unique=False, nullable=False)
+    cost_in_credits = db.Column(db.String, unique=False, nullable=False)
+    hyperdrive_rating = db.Column(db.String, unique=False, nullable=False)
+    manufacturer = db.Column(db.String, unique=False, nullable=False)
+    starship_class = db.Column(db.String, unique=False, nullable=False)
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'name': self.name,
+                'passengers': self.passengers,
+                'crew': self.crew,
+                'length': self.length,
+                'consumables': self.consumables,
+                'cargo_capacity': self.cargo_capacity,
+                'cost_in_credits': self.cost_in_credits,
+                'hyperdrive_rating': self.hyperdrive_rating,
+                'manufacturer': self.manufacturer,
+                'starship_class': self.starship_class}
